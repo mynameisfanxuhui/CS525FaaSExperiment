fd15515cf159b25fa83a3f8e0489c1b5ffcc4a36
mynameisfanxuhui <535192593@qq.com>
2022-04-20 16:40:48 -0400

    add box plot and more data

--- draw_code/draw_result.py
+++ draw_code/draw_result.py
@@ -4,7 +4,7 @@ import numpy as np
 from statistics import mean
 
 dataDict = {}
-applicationList = ["dynamic_html", "thumbnailer"]
+applicationList = ["dynamic_html", "thumbnailer_aws", "uploader_aws", "compression_aws"]
 for appName in applicationList:
     currentDataDict = {}
     maxTime = 0
@@ -23,12 +23,16 @@ for appName in applicationList:
                 rowNumber += 1
                 continue
             memory, type, isCold, executeTime, _, _, _, _ = lines
+            if int(memory) == 128:
+                continue
             minTime = min(minTime, int(executeTime))
             maxTime = max(maxTime, int(executeTime))
+            # turn the ms to s
+            executeTime = int(executeTime) / (10**6)
             if isCold == "False":
-                warm_execution_time.append(int(executeTime))
+                warm_execution_time.append(executeTime)
             else:
-                cold_execution_time.append((int(executeTime)))
+                cold_execution_time.append(executeTime)
     currentDataDict["maxTime"] = maxTime
     currentDataDict["minTime"] = minTime
     currentDataDict["cold_execution_time"] = cold_execution_time
@@ -36,7 +40,7 @@ for appName in applicationList:
     dataDict[appName] = currentDataDict
 
 
-appName = "thumbnailer"
+appName = "compression_aws"
 cold_execution_time = dataDict[appName]["cold_execution_time"]
 warm_execution_time = dataDict[appName]["warm_execution_time"]
 print("this is which application", appName)
@@ -57,21 +61,31 @@ multiMediaAverageWarmTime = mean(warm_execution_time)
 
 
 
-appName = "dynamic_html"
-cold_execution_time = dataDict[appName]["cold_execution_time"]
-warm_execution_time = dataDict[appName]["warm_execution_time"]
-webAppAverageColdTime = mean(cold_execution_time)
-webAppAverageWarmTime = mean(warm_execution_time)
-keys = ['Web_Warm', 'Web_Cold', 'Media_Warm', 'Media_Cold']
-values = [webAppAverageWarmTime, webAppAverageColdTime, multiMediaAverageWarmTime, multiMediaAverageColdTime]
-
-fig = plt.figure(figsize=(10, 5))
-
-# creating the bar plot
-plt.bar(keys, values, color='maroon',
-        width=0.4)
-
-plt.xlabel("type")
-plt.ylabel("Execution Time (Seconds)")
-plt.title("Different type of application's execution time")
+# draw box plot
+# Creating plot
+plt.boxplot(warm_execution_time)
+plt.xlabel("AWS with 256MB memory size + (%s)"%(appName))
+plt.ylabel("warm executed time(seconds)")
+# show plot
 plt.show()
+
+
+
+# appName = "dynamic_html"
+# cold_execution_time = dataDict[appName]["cold_execution_time"]
+# warm_execution_time = dataDict[appName]["warm_execution_time"]
+# webAppAverageColdTime = mean(cold_execution_time)
+# webAppAverageWarmTime = mean(warm_execution_time)
+# keys = ['Web_Warm', 'Web_Cold', 'Media_Warm', 'Media_Cold']
+# values = [webAppAverageWarmTime, webAppAverageColdTime, multiMediaAverageWarmTime, multiMediaAverageColdTime]
+#
+# fig = plt.figure(figsize=(10, 5))
+#
+# # creating the bar plot
+# plt.bar(keys, values, color='maroon',
+#         width=0.4)
+#
+# plt.xlabel("type")
+# plt.ylabel("Execution Time (Seconds)")
+# plt.title("Different type of application's execution time")
+# plt.show()
